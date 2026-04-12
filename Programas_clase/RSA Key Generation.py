c27549c1e17eed4d0c03b0b8964503ed7def0adf
Valeria_García_Herrera <valeriagh@ciencias.unam.mx>
2026-04-12 00:36:31 -0600

    Update RSA Key Generation.py

--- Programas_clase/RSA Key Generation.py
+++ Programas_clase/RSA Key Generation.py
@@ -32,9 +32,9 @@ q = 503
 n = p * q
 phi = (p - 1) * (q - 1)
 
-# Elegimos un 'e' que sea coprimo con phi. 
+# Elegimos un 'e' que sea coprimo con phi.
 # 997 es una excelente opción para este caso.
-e = 997 
+e = 997
 
 d = mod_inverse(e, phi)
 
@@ -49,11 +49,11 @@ else:
 
     # --- 2. CIFRADO ---
     # El mensaje M debe ser menor que n (123 < 177559)
-        mensaje_original = 123
-    
+    mensaje_original = 123
+
     # C = M^e mod n
     cifrado = pow(mensaje_original, e, n)
-    
+
     print(f"Mensaje original: {mensaje_original}")
     print(f"Mensaje cifrado (enviado por la red): {cifrado}")
     print("-" * 40)
@@ -61,7 +61,7 @@ else:
     # --- 3. DESCIFRADO ---
     # M = C^d mod n
     descifrado = pow(cifrado, d, n)
-    
+
     print(f"Mensaje descifrado (con llave privada): {descifrado}")
 
     # Verificación final
