700888d000267c0f3b70fc9b19bc6ea20443e296
Valeria_García_Herrera <valeriagh@ciencias.unam.mx>
2026-04-12 00:32:46 -0600

    Update Aplicación del DLT y The Pinhole Camera Model.py

--- Programas_clase/Aplicación del DLT y The Pinhole Camera Model.py
+++ Programas_clase/Aplicación del DLT y The Pinhole Camera Model.py
@@ -1,14 +1,10 @@
-# --------------------------------
-# Autora: García Herrera Valeria
-# Fecha: 06 - marzo - 2026
-# --------------------------------
+
 
 import numpy as np
 
-# -----------------------
+# ---------------------------
 # 1. Puntos 3D (en cm)
-# -----------------------
-
+# ---------------------------
 M = np.array([
     [0,0,0,1],
     [26.3,0,0,1],
@@ -21,7 +17,6 @@ M = np.array([
 # ---------------------------
 # 2. Puntos 2D (en pixeles)
 # ---------------------------
-
 m = np.array([
     [852, 1583],
     [2459,1500],
@@ -31,44 +26,39 @@ m = np.array([
     [2624,833]
 ])
 
-# --------------------------------
-# 3. Construcción de la matriz A
-# --------------------------------
-
+# ---------------------------
+# 3. Construcción de A
+# ---------------------------
 A = []
 
 for i in range(len(M)):
     X, Y, Z, W = M[i]
     u, v = m[i]
-    
+
     A.append([X, Y, Z, W, 0, 0, 0, 0, -u*X, -u*Y, -u*Z, -u])
     A.append([0, 0, 0, 0, X, Y, Z, W, -v*X, -v*Y, -v*Z, -v])
 
 A = np.array(A)
 
-# -----------
+# ---------------------------
 # 4. SVD
-# -----------
-
+# ---------------------------
 U, S, Vt = np.linalg.svd(A)
 
 # Última fila de V^T
-
 p = Vt[-1]
 
-# -----------------
+# ---------------------------
 # 5. Matriz P
-# -----------------
-
+# ---------------------------
 P = p.reshape(3,4)
 
 print("Vector p:\n", p)
 print("\nMatriz P:\n", P)
 
-# --------------------
+# ---------------------------
 # 6. Verificación
-# --------------------
-
+# ---------------------------
 print("\nVerificación de reproyección:\n")
 
 error_total = 0
@@ -76,13 +66,13 @@ error_total = 0
 for i in range(len(M)):
     proj = P @ M[i]
     proj = proj / proj[2]  # normalización
-    
+
     u_est, v_est = proj[0], proj[1]
     u_real, v_real = m[i]
-    
+
     error = np.sqrt((u_est - u_real)**2 + (v_est - v_real)**2)
     error_total += error
-    
+
     print(f"Punto {i+1}:")
     print(f" Real: ({u_real:.2f}, {v_real:.2f})")
     print(f" Estimado: ({u_est:.2f}, {v_est:.2f})")
