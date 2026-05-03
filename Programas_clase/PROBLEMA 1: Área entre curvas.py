f6ac685d9d833502b488d044072eb5ad35384835
Valeria_García_Herrera <valeriagh@ciencias.unam.mx>
2026-05-02 20:03:10 -0600

    Add script to calculate area between curves

---
+++ Programas_clase/PROBLEMA 1: Área entre curvas.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ---------------------------------------------------
+# PROBLEMA 1: Área entre curvas
+# y = sec^2(x)
+# y = 8cos(x)
+# x ∈ [-pi/3, pi/3]
+# ---------------------------------------------------
+
+x = np.linspace(-np.pi/3, np.pi/3, 1000)
+
+y1 = 1 / (np.cos(x)**2)   # sec^2(x)
+y2 = 8 * np.cos(x)
+
+plt.figure(figsize=(8,5))
+plt.plot(x, y1, label=r'$y=\sec^2(x)$')
+plt.plot(x, y2, label=r'$y=8\cos(x)$')
+
+# sombrear área
+plt.fill_between(x, y1, y2, where=(y2 >= y1), alpha=0.3)
+
+plt.title('Área entre curvas')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.legend()
+plt.show()
