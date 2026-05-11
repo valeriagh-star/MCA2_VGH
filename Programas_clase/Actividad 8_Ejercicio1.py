e96cd8ef9c930ec8772ebe652ebab99f221e3316
Valeria_García_Herrera <valeriagh@ciencias.unam.mx>
2026-05-11 17:12:18 -0600

    Add author and date comments to Actividad 8_Ejercicio1.py
    Added author and date comments to the script.

--- Programas_clase/Actividad 8_Ejercicio1.py
+++ Programas_clase/Actividad 8_Ejercicio1.py
@@ -1,4 +1,7 @@
-
+# --------------------------------
+# Autora: García Herrera Valeria
+# Fecha: 11 - mayo - 2026
+# --------------------------------
 
 import numpy as np
 import matplotlib.pyplot as plt
